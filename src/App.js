40cf890fd5d262126801f5b2d539af15f29c9684
Dom58 <dndahimana58@gmail.com>
2022-11-02 15:37:06 +0200

    setup react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './stories/Header';
+import { Banner } from './stories/Banner';
 
 function App() {
+  const [user, setUser] = React.useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header user={user} onLogin={() => setUser({ name: 'Dom58' })}/>
+      
+      <Banner size={'medium'} backgroundColor='red'>
+        Hello There
+      </Banner>
     </div>
   );
 }
