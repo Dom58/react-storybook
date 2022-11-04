169747326293764fd5523a3268e695fcf8ddb80b
Dom58 <dndahimana58@gmail.com>
2022-11-04 16:15:21 +0200

    create separator stories

--- src/stories/separator/Separator.jsx
+++ src/stories/separator/Separator.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './separator';
 
-export const Separator = ({ backgroundColor, size, content, imageSection, imageUrl, ...props }) => {
+export const Separator = ({ backgroundColor, size, ...props }) => {
     const mode = 'separator';
     const color = color ? color : 'gray';
 
     return (
       <div style={[backgroundColor && { backgroundColor }, {color}]}
-      className={['banner-size', `banner-size--${size}`, mode].join(' ')}
+      className={['separator', `separator-size--${size}`, mode].join(' ')}
       {...props}
       >
       </div>
